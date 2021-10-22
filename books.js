7c19aacd2845f1fb2a9f3ea172203f9d6f8730ad
lokesh <tglokeshbabu@gmail.com>
2021-10-23 00:41:06 +0530

    Heroku deployment

--- books.js
+++ books.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const fs = require('fs');
 var _ = require('lodash');
 var _c = require('lodash-contrib');
@@ -22,7 +21,7 @@ function initialise() {
 	books = fs.readdirSync(booksDir)
 		.filter(file => file.endsWith(".json"))
 		.reduce(function(bks, file){
-				bks[file] = JSON.parse( fs.readFileSync(booksDir + '\\' + file, 'UTF-8'));
+				bks[file] = JSON.parse( fs.readFileSync(booksDir + '/' + file, 'UTF-8'));
 				return bks;
 		}, {});
 }
@@ -41,19 +40,16 @@ module.exports.filterBooks = function (term) {
 	.slice(0, 10)
 	.value()
 	
-	
-	return books;
+
 };
 
 module.exports.getBook = function (bookName) {
 	
-	var book = 
-	_.chain(books)
+	return _.chain(books)
 	.flatMap(booksCollections => booksCollections)
 	.filter(book => _c.strContains(book["BOOK NAME"], bookName))
 	.slice(0, 1)
 	.head()
-	.value()
-	
-	return book;
+	.value();
+
 };
